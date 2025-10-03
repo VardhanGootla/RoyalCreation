9568dfcc217c8ac0ba8a7498d78ce40a97f07383
Sumit Lone <sumitlone1912@gmail.com>
2025-10-03 10:05:11 +0530

    database changes

--- RoyalCreation-main/backend/routes/userRoutes.js
+++ RoyalCreation-main/backend/routes/userRoutes.js
@@ -57,4 +57,18 @@ router.post('/login', async (req, res) => {
   }
 });
 
+// @route   GET api/users
+// @desc    Get all users
+// @access  Public
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
+
